4f3309abe8d3baafc4530642aef2775d14272475
Andrea Baldo <andreabaldo@Andreas-MacBook-Pro.local>
2016-04-12 13:53:15 +0200

    logic working

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
 $(function(){
-
-        var view = {
-        init: function() {
-            this.noteList = $('#notes');
-            var newNoteForm = $('#new-note-form');
-            var newNoteContent = $('#new-note-content');
-            newNoteForm.submit(function(e){
-                itemController.addNewNote(newNoteContent.val());
-                newNoteContent.val('');
-                e.preventDefault();
-            });
-            view.render();
-        },
-        render: function(){
-            var htmlStr = '';
-            itemController.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
-                    note.content + ' ' +
-                    note.date +
-                    '</li>';
-            });
-            this.noteList.html( htmlStr );
-        }
-    };
-
-
+    
     itemController.init();
 });
---
+++ views/views.js
@@ -0,0 +1,26 @@
+/**
+ * Created by andreabaldo on 12/04/16.
+ */
+var view = {
+    init: function() {
+        this.noteList = $('#notes');
+        var newNoteForm = $('#new-note-form');
+        var newNoteContent = $('#new-note-content');
+        newNoteForm.submit(function(e){
+            itemController.addNewNote(newNoteContent.val());
+            newNoteContent.val('');
+            e.preventDefault();
+        });
+        view.render();
+    },
+    render: function(){
+        var htmlStr = '';
+        itemController.getNotes().forEach(function(note){
+            htmlStr += '<li class="note">'+
+                note.content + ' ' +
+                note.date +
+                '</li>';
+        });
+        this.noteList.html( htmlStr );
+    }
+};
